d9d5d3eb6a73d59cc960e4100ebef1332d176e60
raxcat <raxcat@gmail.com>
2016-03-25 11:03:41 +0800

    New simulation UI

--- Example/MusicBarAnimator/StandaloneViewController.swift
+++ Example/MusicBarAnimator/StandaloneViewController.swift
@@ -12,51 +12,10 @@ class StandaloneViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let panGesture = UIPanGestureRecognizer(target: self, action: "detailPanned:")
-        self.view.addGestureRecognizer(panGesture)
 
     }
 
     @IBOutlet weak var imageview: UIImageView!
 
     
-    func detailPanned(recoginzer:UIPanGestureRecognizer){
-        
-        if recoginzer.state == .Began {
-            interactiveTransition = UIPercentDrivenInteractiveTransition()
-            interactiveTransition?.completionCurve = .Linear
-            
-        }
-        
-        guard let view = recoginzer.view else {return}
-        guard let superview = view.superview else {return}
-        
-        let pointInFrame = recoginzer.translationInView(superview)
-        var height = superview.bounds.height
-        
-        
-        var progress = 0 - (pointInFrame.y / ( CGFloat(height) * 1.0))
-        progress = min(1.0, max(0.0, progress));
-        print(height)
-        switch recoginzer.state {
-        case .Began:
-            
-        case .Changed:
-            
-        case .Ended:
-            fallthrough
-        case .Cancelled:
-            pointInBounds = CGPointZero
-            if progress > 0.5 {
-                interactiveTransition?.finishInteractiveTransition()
-            }else{
-                interactiveTransition?.cancelInteractiveTransition()
-            }
-            interactiveTransition = nil
-            
-        default:
-            pointInBounds = CGPointZero
-            
-        }
-    }
 }
